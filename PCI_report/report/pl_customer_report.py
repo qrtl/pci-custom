c91a7278ec4d8b1c883f4165e9cdee6988df25f5
roomsfor <roomsfor@roomsfor-VirtualBox.(none)>
2014-12-15 02:27:16 +0800

    update PCI_report

--- PCI_report/report/pl_customer_report.py
+++ PCI_report/report/pl_customer_report.py
@@ -1,6 +1,5 @@
 # -*- encoding: utf-8 -*-
 from datetime import datetime
-from datetime import date
 import time
 from report import report_sxw
 import pooler
@@ -8,8 +7,9 @@ import logging
 import netsvc
 import tools
 from tools import amount_to_text_en
-import logging
-import time
+import copy
+from collections import Counter
+
 _logger = logging.getLogger(__name__)
 
 
@@ -135,14 +135,16 @@ class Parser(report_sxw.rml_parse):
 
     def _get_eff_periods(self, cr, uid, year_id, context=None):
         res = 0
-#        fiscalyear_obj = self.pool.get('account.fiscalyear')
         today = datetime.today().strftime('%Y-%m-%d')
-#        period_obj = self.pool.get('account.period')
         periods = self.pool.get('account.period').search(cr, uid, [('fiscalyear_id','=',year_id),('date_start','<=',today)], count=True)
         res = periods
         return res
     
-    def printSale(self):
+    def printSale(self, data):
+        res = []
+        page = {}
+        lines = []
+        sumline = []
         cr = self.cr
         uid = self.uid
 
@@ -160,22 +162,6 @@ class Parser(report_sxw.rml_parse):
         if period_ids:
             pe_ids = str(tuple(period_ids))
 
-#        year = self.pool.get('account.fiscalyear').browse(self.cr, self.uid, [year_id])[0].name
-#        monty = 12
-#        years = str(time.localtime()[0])
-#        if year in years:
-#            monty = time.localtime()[1] or 12
-
-        #prev_periods = None
-        #if year_id - 1 != 0:
-        #    period_ids1 = period_obj.search(cr,uid,[('fiscalyear_id','=',year_id - 1)])
-        #    prev_periods = ("%s" % period_ids1).replace('[', '(').replace(']', ')')
-        #    if prev_periods == "()":
-        #        prev_periods = "(0)"
-
-        
-        
-        # get the periods of the previous FY
 
         # get sales data
         if not sale_id:
@@ -204,11 +190,8 @@ class Parser(report_sxw.rml_parse):
                 """ % (pe_ids, sale_id)
             
         cr.execute(sql)
-        #res = cr.dictfetchall()
         sales_data = cr.dictfetchall()
 
-        lines = []  # lines for output
-#        pre_partner = False
         pre_partner1 = False
         line = None 
         total = 0  # sales total of a customer
@@ -254,7 +237,7 @@ class Parser(report_sxw.rml_parse):
                     'period12': 0,
                     'total': 0,
                     'avg_curr_year': 0,
-                    'prev_period': 0,
+                    'prev_fy': 0,
                     'avg_prev_year': 0,
                     'ratio': 0,
                     }
@@ -263,48 +246,79 @@ class Parser(report_sxw.rml_parse):
             vals[i][ln['period']] += ln['amount']
             vals[i]['total'] += ln['amount']
             vals[i]['avg_curr_year'] = vals[i]['total'] / eff_periods
-            vals[i]['avg_prev_year'] = vals[i]['prev_period'] / 12
-            #lines.append(vals)
+            if vals[i]['prev_fy']:
+                vals[i]['avg_prev_year'] = vals[i]['prev_fy'] / 12
+                vals[i]['ratio'] = vals[i]['avg_curr_year'] / vals[i]['avg_prev_year']
+
         # only append values (without key) to form the list
         for k, v in vals.iteritems():
             lines.append(v)
 
         lines = sorted(lines, key=lambda k: k['total'], reverse=True)
-
-        line = [u'Top 100 Total','','',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        line1 = [u'Sales Total','','',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        line2 = [u'Top 100 Ratio','','',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        for lin in lines:
-            for i in range(3,18):
-                line1[i] += lin[i]
         
-        if show:
-            if len(lines) >100:
-                lines = lines[0:100]
-            len_lines = len(lines)
-            for lin in lines:
-                for i in range(3,18):
-                    line[i] += lin[i]
+        top_lines = []
+        if show == True:
+            for line in lines[:100]:
+                top_lines.append(line)
+            page['lines'] = top_lines
         else:
-            m_len = 100
-            if len(lines) < 100:
-                m_len = len(lines)
-            for i in range(m_len):
-                for j in range(3,18):
-                    line[j] += lines[i][j]
+            page['lines'] = lines
+    
+        copy_lines = copy.deepcopy(lines)
+        c = Counter()
+        for d in copy_lines[:100]:  # loop 100 times
+            d['cust_name'] = ''
+            del d['country']
+            del d['is_company']
+            d['ratio'] = 0
+            c.update(d)
+        top100_vals = dict(c)
+        top100_vals['cust_name'] = 'Top-100 Total: '
+        if top100_vals['prev_fy']:
+            top100_vals['ratio'] = top100_vals['avg_curr_year'] / top100_vals['avg_prev_year'] 
+        sumline.append(top100_vals)
 
-        for i in range(3,17):
-            if line1[i] != 0.0:
-                line2[i] = str(round(line[i] / line1[i],2)*100) + "%"
-            else:
-                line2[i] = str(round(line[i],2))+"%"
-
-        lines.append(line)
-        lines.append(line1)
-        lines.append(line2)
+        for d in copy_lines[100:]:  # loop from 101th element
+            d['cust_name'] = ''
+            del d['country']
+            del d['is_company']
+            d['ratio'] = 0
+            c.update(d)
+        total_vals = dict(c)
+        total_vals['cust_name'] = 'All-Customer Total: '
+        if total_vals['prev_fy']:
+            total_vals['ratio'] = total_vals['avg_curr_year'] / total_vals['avg_prev_year'] 
+        sumline.append(total_vals)
         
-        len_line=len(lines)
-        for i in range(len_line):
-            if lines[i][17] != 0:
-                lines[i][18]=str(round(lines[i][16] / lines[i][17],2)*100)+"%"
-        return lines
+        page['sumline'] = sumline
+
+        ratioline = []
+        ratio_vals = {
+            'cust_name': 'Ratio: ',
+            'period1': top100_vals['period1'] / total_vals['period1'],
+            'period2': top100_vals['period2'] / total_vals['period2'],
+            'period3': top100_vals['period3'] / total_vals['period3'],
+            'period4': top100_vals['period4'] / total_vals['period4'],
+            'period5': top100_vals['period5'] / total_vals['period5'],
+            'period6': top100_vals['period6'] / total_vals['period6'],
+            'period7': top100_vals['period7'] / total_vals['period7'],
+            'period8': top100_vals['period8'] / total_vals['period8'],
+            'period9': top100_vals['period9'] / total_vals['period9'],
+            'period10': top100_vals['period10'] / total_vals['period10'],
+            'period11': top100_vals['period11'] / total_vals['period11'],
+            'period12': top100_vals['period12'] / total_vals['period12'],
+            'total': top100_vals['total'] / total_vals['total'],
+            'avg_curr_year': top100_vals['period1'] / total_vals['period1'],
+            'prev_fy': 0,
+            'avg_prev_year': 0,
+            'ratio': 0,
+            }
+        if total_vals['prev_fy']:
+            ratio_vals['avg_prev_year'] = top100_vals['avg_prev_year'] / total_vals['avg_prev_year']
+            ratio_vals['ratio'] = ratio_vals['avg_curr_year'] - ratio_vals['avg_prev_year']
+        ratioline.append(ratio_vals)
+        page['ratioline'] = ratioline
+        
+        res.append(page)
+
+        return res
