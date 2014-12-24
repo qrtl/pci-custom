7d0039a90869252bfd6125f44d522f1c944ba7f0
janicengyy <janice@roomsfor.hk>
2014-12-24 14:54:48 +0800

    remove module

--- stock_inventory_projection_report/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-import report
-import wizard
-
-
--- stock_inventory_projection_report/wizard/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import inventory_projection_wizard
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
-
