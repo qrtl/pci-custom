b3ed396abb9fa56855014f422059161770d41db3
Yoshi Tashiro <roomsfor@roomsfor-VirtualBox.(none)>
2015-02-28 15:49:23 +0800

    [UPD] add quotation in shipment qty calc

--- stock_shipment_schedule_report/report/shipment_schedule_report.py
+++ stock_shipment_schedule_report/report/shipment_schedule_report.py
@@ -7,6 +7,7 @@ from report import report_sxw
 import logging
 import pytz
 from openerp import SUPERUSER_ID
+from collections import Counter
 from openerp.tools.translate import _
 
 _logger = logging.getLogger(__name__)
@@ -18,13 +19,6 @@ class Parser(report_sxw.rml_parse):
         self.localcontext.update( {
             'print_schedule':self.print_schedule,
         })
-
-#    def set_context(self,objects, data, ids, report_type=None):
-#        self.localcontext.update({
-#            'threshold_date': data.get('threshold_date', False),
-#            'category_id': data.get('category_id', False),
-#        })
-#        return super(Parser, self).set_context(objects, data, ids, report_type)
 
     def _get_user_tz(self, cr, uid, context=None):
         user = self.pool.get('res.users').browse(cr, SUPERUSER_ID, uid)
@@ -121,7 +115,55 @@ class Parser(report_sxw.rml_parse):
             raise osv.except_osv(_('Warning!'), _("There is no product to meet the condition (i.e. 'Saleable', 'Stockable Product' and belong to the selected Product Category or its offsprings)."))
         return prod_ids
  
-    def _get_move_qty_data(self, cr, uid, product_ids, periods, line_vals, context=None):
+    def _get_move_qty_data(self, cr, uid, params, context=None):
+        res = {}
+        sql = """
+            select m.product_id, sum(m.product_qty / u.factor)
+            from stock_move m
+            left join product_uom u on (m.product_uom = u.id)
+            where location_id %s %s
+            and location_dest_id %s %s
+            and product_id IN %s
+            and state NOT IN ('done', 'cancel')
+            and m.date >= %s
+            and m.date < %s
+            group by product_id
+            """ % (tuple(params))
+        cr.execute(sql)
+        qty_dict = cr.dictfetchall()
+        for rec in qty_dict:
+            res[rec['product_id']] = rec['sum']
+        return res
+ 
+    def _get_quote_qty_data(self, cr, uid, params, context=None):
+        res = {}
+        sql = """
+            select ol.product_id, sum(ol.product_uom_qty / u.factor)
+            from sale_order_line ol
+            left join product_uom u on (ol.product_uom = u.id)
+            where product_id IN %s
+            and order_id in
+                (select id
+                from sale_order
+                where state = 'draft'
+                and date_order >= '%s'
+                and date_order < '%s')
+            group by product_id
+            """ % (tuple(params))
+        cr.execute(sql)
+        qty_dict = cr.dictfetchall()
+        for rec in qty_dict:
+            res[rec['product_id']] = rec['sum']
+        return res
+ 
+    def _get_dates4quote(self, cr, uid, period, context=None):
+        res = {}
+        usertz = self._get_user_tz(cr, uid, context=None)
+        res['start'] = period['start'].astimezone(usertz).strftime('%Y-%m-%d')
+        res['end'] = period['end'].astimezone(usertz).strftime('%Y-%m-%d')
+        return res
+    
+    def _get_qty_data(self, cr, uid, product_ids, periods, line_vals, context=None):
         res = []
         param_prod_ids = str(product_ids).replace('[', '(').replace(']', ')')  # this conversion (instead of 'tuple(product_ids,)' is in case there is only one product)
         int_loc_ids = self.pool.get('stock.location').search(cr, uid, [('usage','=','internal')])
@@ -129,29 +171,19 @@ class Parser(report_sxw.rml_parse):
         for _ in xrange(7):
             date_from = "'"+str(periods[i]['start'])+"'"
             date_to = "'"+str(periods[i]['end'])+"'"
+            in_params = ['NOT IN', tuple(int_loc_ids,), 'IN', tuple(int_loc_ids,), param_prod_ids, date_from, date_to]
+            out_params = ['IN', tuple(int_loc_ids,), 'NOT IN', tuple(int_loc_ids,), param_prod_ids, date_from, date_to]
+            dates4quote = self._get_dates4quote(cr, uid, periods[i], context=context)
+            quote_params = [param_prod_ids, dates4quote['start'], dates4quote['end']]
             for what in ['in', 'out']:
                 if what == 'in':
-                    params = ['NOT IN', tuple(int_loc_ids,), 'IN', tuple(int_loc_ids,), param_prod_ids, date_from, date_to]
-                    tuple_params = tuple(params)
+                    qty_data = self._get_move_qty_data(cr, uid, in_params, context=context)
                 else:
-                    params = ['IN', tuple(int_loc_ids,), 'NOT IN', tuple(int_loc_ids,), param_prod_ids, date_from, date_to]
-                sql = """
-                    select m.product_id, sum(m.product_qty / u.factor)
-                    from stock_move m
-                    left join product_uom u on (m.product_uom = u.id)
-                    where location_id %s %s
-                    and location_dest_id %s %s
-                    and product_id IN %s
-                    and state NOT IN ('done', 'cancel')
-                    and m.date >= %s
-                    and m.date < %s
-                    group by product_id
-                    """ % (tuple(params))
-                cr.execute(sql)
-                data = cr.dictfetchall()
-                for rec in data:
-                    prod = rec['product_id']
-                    line_vals[prod][what+`i`] = rec['sum'] 
+                    move_qty_data = self._get_move_qty_data(cr, uid, out_params, context=context)
+                    quote_qty_data = self._get_quote_qty_data(cr, uid, quote_params, context=context)
+                    qty_data = dict(Counter(move_qty_data) + Counter(quote_qty_data))
+                for k, v in qty_data.iteritems():
+                    line_vals[k][what+`i`] = v
             i += 1
 #         del_prod_list = []
         for prod in line_vals:  # identify products not to display (if no shipment, no display)
@@ -208,7 +240,7 @@ class Parser(report_sxw.rml_parse):
                 'out6': 0.0,
                 'bal6': 0.0,
             }
-        lines = self._get_move_qty_data(cr, uid, product_ids, periods, line_vals, context=None)
+        lines = self._get_qty_data(cr, uid, product_ids, periods, line_vals, context=None)
         for line in lines:  # only append values (without key) to form the list
             for k, v in line.iteritems():
                 res.append(v)
