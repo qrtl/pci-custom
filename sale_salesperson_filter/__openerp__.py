6ca39e9c04a101abd6ba829ba1581b93b55a2215
Manami Hashi <manami@roomsfor.hk>
2016-02-01 16:05:26 +0800

    [ADD] create module

---
+++ sale_salesperson_filter/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2016 Rooms For (Hong Kong) Limited T/A OSCG
+#    <https://www.odoo-asia.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+{
+    'name': 'Sales Order Salesperson Filter',
+    'summary':"""""",
+    'version': '7.0.0.5.0',
+    'category': 'Sales',
+    'description': """ 
+* Do not propose Salesperson as login user by default
+* Add a boolean field to Users on whether be proposed as Salesperson
+    """,
+    'author': 'Rooms For (Hong Kong) Limited T/A OSCG',
+    'website': 'https://www.odoo-asia.com',
+    'license': 'AGPL-3',
+    'depends': ['sale',],
+    'data': [
+        'views/res_users_view.xml',
+        'views/sale_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
