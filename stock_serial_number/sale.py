7e7819c92b6a9eb760d664757233f87fc19bd414
Manami Hashi <manami@roomsfor.hk>
2016-06-14 06:57:26 +0000

    [UPD] update view to change onchange

--- stock_serial_number/sale.py
+++ stock_serial_number/sale.py
@@ -44,10 +44,8 @@ class sale_order_line(osv.osv):
 
         res = super(sale_order_line, self).product_id_change(cr, uid, ids,
             pricelist=pricelist, product=product, qty=qty,
-            uom=uom, qty_uos=qty_uos, uos=uos, name=name,
-            partner_id=partner_id,
-            lang=lang, update_tax=update_tax, date_order=date_order,
-            packaging=packaging,
+            uom=uom, qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id,
+            lang=lang, update_tax=update_tax, date_order=date_order, packaging=packaging,
             fiscal_position=fiscal_position, flag=flag, context=context)
         if serial_id:
             lot_obj = self.pool.get('stock.production.lot').browse(cr, uid, serial_id, context)
@@ -55,6 +53,7 @@ class sale_order_line(osv.osv):
                 res['value'].update({'serial_id': False})
             res['value'].update({'price_unit': lot_obj.list_price})
         return res
+
 
 #    def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
 #            uom=False, qty_uos=0, uos=False, name='', partner_id=False,
