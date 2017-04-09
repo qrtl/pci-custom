20b3c574c7836f62edbdb3bd2521e58b526f7d8e
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-04-09 12:47:16 +0800

    Stock inventory product type (#19)
    * [ADD] stock_inventory_product_type
    
    * [FIX] license

---
+++ stock_inventory_product_type/models/stock_quant.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Rooms For (Hong Kong) Limited T/A OSCG
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from openerp import api, fields, models
+
+
+class StockQuant(models.Model):
+    _inherit = 'stock.quant'
+    
+    product_type = fields.Selection(
+        related='product_id.product_tmpl_id.type',
+        store=True,
+        string="Product Type"
+    )
