f519f996db11e8a9ab341529ed5dbe2914db60a6
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-04-28 16:59:18 +0800

    [ADD] sale_logic_adj_pci

---
+++ sale_logic_adj_pci/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Rooms For (Hong Kong) Limited T/A OSCG
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Adjustments on Sales Logic',
+    'summary':"""""",
+    'version': '10.0.1.0.0',
+    'category': 'Sales',
+    'description': """
+* Not propose SO line name from product.description_sale
+    """,
+    'author': 'Rooms For (Hong Kong) Limited T/A OSCG',
+    'website': 'https://www.odoo-asia.com',
+    'license': 'LGPL-3',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
