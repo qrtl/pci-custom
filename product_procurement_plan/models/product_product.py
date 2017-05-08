6570297b26d54e4f2ec165b391fd4796b9f5143a
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-05-08 16:50:31 +0800

    [IMP] delete comments

--- product_procurement_plan/models/product_product.py
+++ product_procurement_plan/models/product_product.py
@@ -42,28 +42,6 @@ class ProductProduct(models.Model):
     )
 
 
-    # def _compute_qty(self, cr, uid, ids, field_names, args, context=None):
-    #     res = {}
-    #     prod_ids = self.search(cr, uid, [])
-    #     for prod in self.browse(cr, uid, prod_ids, context=context):
-    #         if prod.avg_qty_adj_comp:
-    #             avg_qty = prod.avg_qty_adj_comp
-    #         elif prod.avg_qty_adj:
-    #             avg_qty = prod.avg_qty_adj
-    #         else:
-    #             avg_qty = prod.avg_qty_needed
-    #         if prod.proc_lt_manu:
-    #             proc_lt = prod.proc_lt_manu
-    #         else:
-    #             proc_lt = prod.proc_lt_calc
-    #         qty_suggested = avg_qty * proc_lt
-    #         qty_variance = prod.qty_available + prod.incoming_qty - qty_suggested
-    #         res[prod.id] = {
-    #             'qty_suggested': qty_suggested,
-    #             'qty_variance': qty_variance,
-    #         }
-    #     return res
-
     @api.multi
     def _compute_qty(self):
         for prod in self:
@@ -77,8 +55,6 @@ class ProductProduct(models.Model):
                 proc_lt = prod.proc_lt_manu
             else:
                 proc_lt = prod.proc_lt_calc
-            # qty_suggested = avg_qty * proc_lt
-            # qty_variance = prod.qty_available + prod.incoming_qty - qty_suggested
             prod.qty_suggested = avg_qty * proc_lt
             prod.qty_variance = prod.qty_available + prod.incoming_qty - \
                                 prod.qty_suggested
