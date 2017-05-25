2162ccd3e085c5187768530235ff2d8c566ebc45
tslai <timlai125@gmail.com>
2017-05-25 16:06:21 +0800

    Modifying Website Sale Cart View

---
+++ website_sale_adj/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'View Adjustments on Website Sale',
+    'version': '10.0.1.0.0',
+    'category': 'Website',
+    'summary': '',
+    'description': """
+* Adjust shopping cart view
+    """,
+    'author': 'Quartile Limited',
+    'license': 'AGPL-3',
+    'website': 'https://www.odoo-asia.com/',
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+        'views/templates.xml'
+    ],
+    'installable': True,
+}
