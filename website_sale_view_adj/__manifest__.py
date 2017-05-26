29b5bbdb6a0e1c773c509a540ed436921768799e
TimLai125 <timlai125@gmail.com>
2017-05-26 11:46:21 +0800

    Modifying Website Sale Cart View (#33)
    * Modifying Website Sale Cart View
    
    * Renaming Module website_sale_adj to website_sale_view_adj
    Correcting the license type and adding comments
    Improving code sytnax
    Modifiying on product's view

---
+++ website_sale_view_adj/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgp1).
+{
+    'name': 'View Adjustments on Website Sale',
+    'version': '10.0.1.0.0',
+    'category': 'Website',
+    'summary': '',
+    'description': """
+* Adjust shopping cart view
+* Adjust product page view
+    """,
+    'author': 'Quartile Limited',
+    'license': 'LGPL-3',
+    'website': 'https://www.odoo-asia.com/',
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+        'views/templates.xml'
+    ],
+    'installable': True,
+}
