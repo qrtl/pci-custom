c06816fd35f2f80260fb26af3739471ebfc18324
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-05-29 18:16:41 +0800

    Stock level forecast (#35)
    * [ADD] stock_level_forecast_report
    
    * misc. updates
    
    * [IMP] further adjust SQL layout
    
    * [IMP] remove default group by with prod categ
    
    * rename module, adjust search_default
    
    * [FIX] remove unnecessary group by

---
+++ stock_level_forecast/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Stock Level Forecast Report',
+    'category': 'Warehouse',
+    'license': 'LGPL-3',
+    'summary': 'Enhanced Stock Level Forecast Report',
+    'author': 'Quartile Limited',
+    'website': 'https://www.odoo-asia.com',
+    'version': '10.0.1.0.0',
+    'description': """
+Adds Can Be Sold and Product Category filters to Stock Level Forecast Report.
+""",
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'report/report_stock_forecast.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
