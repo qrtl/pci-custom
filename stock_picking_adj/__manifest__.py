9856b60e58c77cef74c582019f948aa921f874a7
tslai <tl@quartile.co>
2017-06-28 03:21:19 +0000

    [ADD] stock_picking_adj

---
+++ stock_picking_adj/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Disable Force Availability',
+    'version': '10.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.odoo-asia.com',
+    'category': 'Stock',
+    'license': "LGPL-3",
+    'description': "",
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/stock_picking_views.xml',
+    ],
+    'installable': True,
+}
