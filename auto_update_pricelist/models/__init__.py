13745117a1c81a36859fe3349934d37a33aaf7c3
tslai <tl@quartile.co>
2017-06-29 06:09:36 +0000

    [FIX] Remove internal reference from product description

--- auto_update_pricelist/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import partner
-import product_pricelist
-import partner_yearly_sales
-import sale
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- auto_update_pricelist/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import update_pricelist_wizard
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
