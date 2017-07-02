42eeb18123555d29c9da6b7466fa4f9c1011a80b
Yoshi Tashiro <tashiro@quartile.co>
2017-07-02 11:37:40 +0000

    remove unused import

--- account_invoice_line_view/models/account_invoice_line.py
+++ account_invoice_line_view/models/account_invoice_line.py
@@ -3,7 +3,6 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import api, fields, models
-import odoo.addons.decimal_precision as dp
 
 
 class AccountInoviceLine(models.Model):
