02965485661e585c4f6854a5898e8e1478b5dd8e
tslai <tl@quartile.co>
2017-07-04 03:16:16 +0000

    [IMP] combining sale_logic_adj_pci into product_description_adj

--- product_description_adj/__manifest__.py
+++ product_description_adj/__manifest__.py
@@ -3,14 +3,17 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     'name': 'Remove Internal Reference',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'author': 'Quartile Limited',
     'website': 'https://www.odoo-asia.com',
     'category': 'Product',
     'license': "LGPL-3",
-    'description': "",
+    'description': """
+Remove the internal reference from product's description in sale orders and invoices.
+""",
     'depends': [
         'product',
+        'sale',
     ],
     'data': [
     ],
