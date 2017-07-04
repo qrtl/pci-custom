9760c3b5718199ab4466492522ae45897c6166c0
TimLai125 <tl@quartile.co>
2017-07-04 11:41:04 +0800

    [ADD] product_description_adj (#48)
    Combining the sale_logic_adj_pci into product_description_adj, remove the internal reference from the product's description in sale orders and customer invoices.

---
+++ product_description_adj/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Remove Internal Reference',
+    'version': '10.0.1.1.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.odoo-asia.com',
+    'category': 'Product',
+    'license': "LGPL-3",
+    'description': """
+Remove the internal reference from product's description in sale orders and invoices.
+""",
+    'depends': [
+        'product',
+        'sale',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
---
+++ product_description_adj/models/product.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.one
+    def _compute_partner_ref(self):
+        product_name = self.name
+        for supplier_info in self.seller_ids:
+            if supplier_info.name.id == self._context.get('partner_id'):
+                product_name = supplier_info.product_name or self.default_code
+        self.partner_ref = '%s' % (product_name)
