dbd6c3643b46cb7ab913d7d0dc921d4529c2f005
tslai <tl@quartile.co>
2017-07-13 09:54:21 +0000

    [ADD] sale_salesperson_filter

---
+++ sale_salesperson_filter/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Sale Order Salesperson Filter',
+    'version': '10.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.odoo-asia.com',
+    'category': 'Sales',
+    'license': "LGPL-3",
+    'description': """
+* Do not propose Saleperson by default when login
+* Add Boolean field to User on whether to propose
+""",
+    'depends': [
+        'sale',
+        'base',
+    ],
+    'data': [
+        "views/sale_order_view.xml",
+        "views/res_partner_view.xml",
+        "views/res_users_view.xml",
+    ],
+    'installable': True,
+}
