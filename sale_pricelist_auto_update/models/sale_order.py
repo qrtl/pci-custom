d5979164ff9e83dcf470d1b81790d0a94913ec8c
Yoshi Tashiro <tashiro@quartile.co>
2017-07-14 14:29:35 +0000

    remove redundant methods

--- sale_pricelist_auto_update/models/sale_order.py
+++ sale_pricelist_auto_update/models/sale_order.py
@@ -9,20 +9,6 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     @api.multi
-    def action_confirm(self):
-        res = super(SaleOrder, self).action_confirm()
-        for order in self:
-            order.partner_id._update_current_pricelist()
-        return res
-
-    @api.multi
-    def action_cancel(self):
-        res = super(SaleOrder, self).action_cancel()
-        for order in self:
-            order.partner_id._update_current_pricelist()
-        return res
-
-    @api.multi
     def write(self, vals):
         res = super(SaleOrder, self).write(vals)
         for order in self:
