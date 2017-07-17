b7c4cf0282d8ece250d220fe51d9501f8e2ceb82
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-07-17 16:47:58 +0800

    [ADD] stock_delivery_notification (#58)

---
+++ stock_delivery_notification/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Delivery Order Notification',
+    'version': '10.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.odoo-asia.com',
+    'category': 'Stock',
+    'license': "LGPL-3",
+    'description': """
+Send email to the customer for online orders when delivery is done.
+""",
+    'depends': [
+        'mail_force_email',
+        'sale_stock',
+        'website_portal_sale',
+    ],
+    'data': [
+        'data/mail_template_data.xml',
+    ],
+    'installable': True,
+}
