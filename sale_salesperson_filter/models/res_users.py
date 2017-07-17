50f6410e8a1ebd4a1ea073a6a84cfe8a740035fd
Tim <tl@quartile.co>
2017-07-17 16:06:58 +0800

    [ADD] sale_salesperson_filter (#54)
    * [ADD] sale_salesperson_filter
    
    * [FIX] fix view issues, add account invoice view
    
    * [FIX] fix __manifest__.py
    
    * [FIX] view bug fix
    
    * [FIX] change views' names and attributes fix
    
    * [FIX] Fix descriptions of __manifest__

---
+++ sale_salesperson_filter/models/res_users.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class Users(models.Model):
+    _inherit = 'res.users'
+
+    salesperson_select = fields.Boolean(
+        string="Salesperson Setectable",
+        help="Indicates the salesperson to be selected in sales order",
+    )
+
+    @api.multi
+    def toggle_salesperson(self):
+        for record in self:
+            record.salesperson_select = not record.salesperson_select
