2cf88af2697496dc7d8b1d56dca178a5b7ca093d
Yoshi Tashiro <tashiro@quartile.co>
2017-07-31 08:49:59 +0000

    [ADD] abstract_report_xlsx

---
+++ abstract_report_xlsx/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Author: Julien Coux
+# Copyright 2016 Camptocamp SA
+# Copyright 2016-2017 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Abstract Report XLSX",
+    "summary": "",
+    "version": "10.0.1.0.0",
+    "category": "Reporting",
+    "website": "https://www.odoo-asia.com/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "report_xlsx",
+    ],
+}
---
+++ abstract_report_xlsx/reports/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import stock_abstract_report_xlsx
