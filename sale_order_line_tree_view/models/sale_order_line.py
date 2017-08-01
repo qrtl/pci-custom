938d4a511ddc87cb52afe87693340a230a1f2d24
tslai <tl@quartile.co>
2017-08-01 08:52:04 +0000

    [FIX] change Expected Date from date to datetime

--- sale_order_line_tree_view/models/sale_order_line.py
+++ sale_order_line_tree_view/models/sale_order_line.py
@@ -8,7 +8,7 @@ from odoo import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    expected_date = fields.Date(
+    expected_date = fields.Datetime(
         readonly=True,
         store=True,
         related='order_id.expected_date'
