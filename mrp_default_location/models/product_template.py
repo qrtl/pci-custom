687d9b5c4450d67e38beaf07698a00099439c59f
Yoshi Tashiro <tashiro@quartile.co>
2017-09-14 15:16:42 +0000

    [ADD] mrp_default_location

---
+++ mrp_default_location/models/product_template.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    mrp_location_src_id = fields.Many2one(
+        comodel_name='stock.location',
+        domain=[('usage', '=', 'internal')],
+        string='Manufacturing Default Source Location'
+    )
