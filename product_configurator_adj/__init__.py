21f97fd49a17347159e878210fc554c02f316c9f
Yoshi Tashiro <tashiro@quartile.co>
2017-09-19 07:26:05 +0000

    [IMP] workaround for many2many_tags widget issue

--- product_configurator_adj/__init__.py
+++ product_configurator_adj/__init__.py
@@ -1,5 +1,1 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Rooms For (Hong Kong) Limited T/A OSCG
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from . import models
--- product_configurator_adj/__manifest__.py
+++ product_configurator_adj/__manifest__.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Rooms For (Hong Kong) Limited T/A OSCG
+# Copyright 2017 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     'name': 'Adjustments on Product Configurator Base',
     'version': '10.0.1.0.0',
     'category': 'Generic Modules/Base',
     'summary': '',
-    'author': 'Rooms For (Hong Kong) Limited T/A OSCG',
+    'author': 'Quartile Limited',
     'license': 'LGPL-3',
     'website': 'https://www.odoo-asia.com',
     'depends': [
         'product_configurator',
     ],
     "data": [
+        'views/product_config_step_line_views.xml',
+        'views/product_template_views.xml',
     ],
-    'demo': [
-    ],
-    'images': [
-    ],
-    'test': [],
     'installable': True,
-    'auto_install': False,
 }
