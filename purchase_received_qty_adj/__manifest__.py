50821e3d85618f9f813e046da9ba8f59ef76a62c
tslai <tl@quartile.co>
2017-09-26 06:57:22 +0000

    [ADD] purchase_received_qty

---
+++ purchase_received_qty_adj/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Showing actual received quantity of Purchase Order',
+    'version': '10.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.odoo-asia.com',
+    'category': 'Purchase',
+    'license': "LGPL-3",
+    'description': """
+- Add new field qty_received_adj
+- Computes the actual quantity received that excluding the quantity in
+return orders
+- Add transfer reference to stock move tree view
+""",
+    'depends': [
+        'purchase',
+        'purchase_stock_picking_return_invoicing',
+    ],
+    'data': [
+        'views/purchase_order_view.xml',
+        'views/stock_move_view.xml',
+    ],
+    'installable': True,
+}
