0c30ab82bd2b04f96d639fc0eb2f1717cb755628
tslai <tl@quartile.co>
2017-10-04 09:34:49 +0000

    [ADD] account_bank_statement_adj

---
+++ account_bank_statement_adj/models/account_bank_statement.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models, fields, api
+
+
+class AccountBankStatementLine(models.Model):
+    _inherit = "account.bank.statement.line"
+
+    @api.multi
+    def button_cancel_reconciliation(self):
+        super(AccountBankStatementLine, self).button_cancel_reconciliation()
+        for st_line in self:
+            st_line.move_name = False
