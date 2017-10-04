84e062db2e27846bb6ddc3417382d4046a2e7d57
Tim <tl@quartile.co>
2017-10-04 17:58:30 +0800

    [ADD] account_bank_statement_adj (#83)

---
+++ account_bank_statement_adj/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Clear move_name when unreconciling bank statement line',
+    'version': '10.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.odoo-asia.com',
+    'category': 'Account',
+    'license': "LGPL-3",
+    'description': """
+When unreconciling bank statement line, the move_name of the bank statement
+line will be cleared.
+""",
+    'depends': [
+        'account',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
