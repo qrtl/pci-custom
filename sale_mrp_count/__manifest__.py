1c6b535df72ec95daf0ecaa4a1c066a2779c809a
Yoshi Tashiro <tashiro@quartile.co>
2017-10-11 09:53:12 +0000

    [ADD] sale_mrp_count

---
+++ sale_mrp_count/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale MRP Count",
+    "summary": "",
+    "version": "10.0.1.0.0",
+    "category": "Sales",
+    "website": "https://www.odoo-asia.com/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_mrp",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
