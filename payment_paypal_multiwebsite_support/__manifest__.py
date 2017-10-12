2d63cba72ff182f93db29eff5ee285af6a7d6a7c
tslai <tl@quartile.co>
2017-10-12 09:28:12 +0000

    [FIX] Remove redundant dependency and adjust name

--- payment_paypal_multiwebsite_support/__manifest__.py
+++ payment_paypal_multiwebsite_support/__manifest__.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Provide mutli-website feature to PayPal Acquirer',
+    'name': 'Extend PayPal Acquirer to support multi-website',
     'version': '10.0.1.0.0',
     'author': 'Quartile Limited',
     'website': 'https://www.odoo-asia.com',
@@ -14,7 +14,6 @@ modify the return url sent to PayPal
 """,
     'depends': [
         'payment_paypal',
-        'pr1_multi_website',
     ],
     'data': [
     ],
