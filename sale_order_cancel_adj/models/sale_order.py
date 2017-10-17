439c50374702ae6029470c5f8436f619144404a7
Tim <tl@quartile.co>
2017-10-17 10:24:26 +0800

    [ADD] sale_order_cancel_adj (#85)
    * [ADD] sale_order_cancel_adj
    
    * [IMP] Add related PR in Odoo repository

---
+++ sale_order_cancel_adj/models/sale_order.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models, fields, api
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    # Related Github PR: https://github.com/odoo/odoo/pull/18121
+    @api.multi
+    def action_cancel(self):
+        self.mapped('picking_ids').action_cancel()
+        return super(SaleOrder, self).action_cancel()
