c505de36256ee94d98f970fc789b13a8c6bcb415
tslai <tl@quartile.co>
2017-10-19 08:22:02 +0000

    [FIX] Improve styling

--- website_product_delivery_method/models/__init__.py
+++ website_product_delivery_method/models/__init__.py
@@ -4,4 +4,4 @@ from . import product_category
 from . import customer_group
 from . import res_partner
 from . import delivery_carrier
-from . import sale
+from . import sale_order
--- website_product_delivery_method/models/product_category.py
+++ website_product_delivery_method/models/product_category.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
 
