4acf4698bd3f56f94e06615b3963ab349313ae5c
Yoshi Tashiro <tashiro@quartile.co>
2017-10-19 07:01:00 +0000

    [ADD] product_view_adj

---
+++ product_view_adj/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Adjustments on Product Related Views',
+    'version': '10.0.1.0.0',
+    'category': 'Generic Modules/Base',
+    'summary': '',
+    'author': 'Quartile Limited',
+    'license': 'AGPL-3',
+    'website': 'https://www.odoo-asia.com',
+    'depends': [
+        'product_configurator',
+    ],
+    "data": [
+        'views/product_attribute_value_views.xml',
+    ],
+    'installable': True,
+}
