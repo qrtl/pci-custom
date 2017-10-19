2fe864f7a1fcf52a0332710ca7cd72baf4152574
Yoshi Tashiro <tashiro@quartile.co>
2017-10-19 07:00:16 +0000

    LGPL -> AGPL

--- product_configurator_adj/__manifest__.py
+++ product_configurator_adj/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Quartile Limited
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Adjustments on Product Configurator Base',
     'version': '10.0.1.0.0',
     'category': 'Generic Modules/Base',
     'summary': '',
     'author': 'Quartile Limited',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'website': 'https://www.odoo-asia.com',
     'depends': [
         'product_configurator',
