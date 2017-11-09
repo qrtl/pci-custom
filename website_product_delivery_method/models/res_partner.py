fc8c36892fc38bca33749ae520920f5dd028d930
tslai <tl@quartile.co>
2017-11-09 06:47:38 +0000

    [FIX] Set End-user as the default customer group

--- website_product_delivery_method/models/res_partner.py
+++ website_product_delivery_method/models/res_partner.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ResPartner(models.Model):
@@ -13,3 +13,14 @@ class ResPartner(models.Model):
         string="Customer Group",
         copy=False,
     )
+
+    @api.model
+    def create(self, vals):
+        if 'customer_group' not in vals:
+            default_group = self.env["customer.group"].search([
+                ('name', '=', 'End-user')
+            ])
+            if default_group:
+                vals['customer_group'] = default_group[0].id
+        user = super(ResPartner, self).create(vals)
+        return user
