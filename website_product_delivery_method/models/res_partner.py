266f8ac444ba704f9e93d42aa6fdce71eae6f63f
Tim <tl@quartile.co>
2017-11-09 16:33:00 +0800

    Remove redundant import

--- website_product_delivery_method/models/res_partner.py
+++ website_product_delivery_method/models/res_partner.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models, api
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
