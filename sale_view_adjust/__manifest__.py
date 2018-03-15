668acfe6802b4c43f6ef62496f1e9de8b5133aba
Tim <tl@quartile.co>
2018-03-15 21:12:46 +0800

    Sales order hides Lock button (#102)

--- sale_view_adjust/__manifest__.py
+++ sale_view_adjust/__manifest__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Quartile Limited
+# Copyright 2017-2018 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     'name': 'Adjustments on Sales Views',
     'summary':"""""",
-    'version': '10.0.1.2.0',
+    'version': '10.0.1.2.1',
     'category': 'Sales',
     'description': """
     """,
     'author': 'Quartile Limited',
-    'website': 'https://www.odoo-asia.com',
+    'website': 'https://www.quartile.co',
     'license': 'LGPL-3',
     'depends': [
         'sale',
