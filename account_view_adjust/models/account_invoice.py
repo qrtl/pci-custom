8cd7ec1790fd8763216178f946823146b906c90f
tslai <tl@quartile.co>
2018-04-26 10:42:24 +0000

    [IMP] Adjust invoice cancel logic

---
+++ account_view_adjust/models/account_invoice.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017-2018 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo.exceptions import UserError
+from odoo import models, fields, api, _
+
+
+class AccountInovice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.multi
+    def action_invoice_cancel(self):
+        if self.filtered(
+                lambda inv: inv.state not in ['proforma2', 'draft', 'open']
+                and inv.state == "paid" and inv.amount_total != 0):
+            raise UserError(_("Invoice must be in draft, Pro-forma or open "
+                              "state or the invoice amount is 0 in order to "
+                              "be cancelled."))
+        return self.action_cancel()
