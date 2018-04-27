8cd5c45f981d985b614313532ed1509955a7ff82
Tim <tl@quartile.co>
2018-04-27 19:28:15 +0800

    [IMP] Adjust invoice cancel logic (#105)

--- account_view_adjust/__manifest__.py
+++ account_view_adjust/__manifest__.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Quartile Limited
+# Copyright 2017-2018 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     'name': 'Adjustments on Accounting Views',
     'summary':"""""",
-    'version': '10.0.1.1.1',
+    'version': '10.0.1.2.0',
     'category': 'Accounting',
     'description': """
     """,
     'author': 'Quartile Limited',
-    'website': 'https://www.odoo-asia.com',
+    'website': 'https://www.quartile.co',
     'license': 'LGPL-3',
     'depends': [
         'account',
+        'account_cancel',
     ],
     'data': [
         'views/account_invoice_views.xml',
