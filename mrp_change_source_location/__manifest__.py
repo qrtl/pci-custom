ad522e481e9c576d9b7364f1ddedfd86243b7a3d
tslai <tl@quartile.co>
2018-08-10 09:15:14 +0000

    [ADD] mrp_change_source_location

---
+++ mrp_change_source_location/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Manufacturing Order Change Source Locations",
+    "summary": "",
+    "description": """
+""",
+    "version": "10.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://www.quartile.co",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        'wizard/change_source_location_wizard_views.xml',
+        'views/mrp_production_views.xml',
+    ],
+}
---
+++ mrp_change_source_location/wizard/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import change_source_location_wizard
