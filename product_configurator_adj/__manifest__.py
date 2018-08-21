5b53c377eb53cb11dcec1627243c30f26a1fa29c
Yoshi Tashiro <tashiro@quartile.co>
2018-08-21 08:43:22 +0000

    [IMP] remove view adjustments https://github.com/rfhk/cip-custom/tree/3f17c81e9180960e7d83d0ae476795e181b63846/fix_product_attribute_line_name_search dismisses the reason why the view adjustments were needed. Therefore, we will revert the changes.

--- product_configurator_adj/__manifest__.py
+++ product_configurator_adj/__manifest__.py
@@ -3,18 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Adjustments on Product Configurator Base',
-    'version': '10.0.1.1.0',
+    'version': '10.0.1.2.0',
     'category': 'Generic Modules/Base',
     'summary': '',
     'author': 'Quartile Limited',
     'license': 'AGPL-3',
-    'website': 'https://www.odoo-asia.com',
+    'website': 'https://www.quartile.co',
     'depends': [
         'product_configurator',
     ],
     "data": [
-        'views/product_config_step_line_views.xml',
-        'views/product_template_views.xml',
     ],
     'installable': True,
 }
