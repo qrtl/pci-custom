cd7b0d3062713412482239ff64a790dd3bbedc62
Yoshi Tashiro <tashiro@quartile.co>
2018-08-21 12:06:18 +0000

    [IMP] move config product mapping list from product template form to an menu item

--- product_configurator_mrp_ext/__manifest__.py
+++ product_configurator_mrp_ext/__manifest__.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016-2017 Pledra
 # Copyright 2017 Willdooit
-# Copyright 2017 Quartile Limited
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# Copyright 2017-2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Product Configurator Manufacturing Extended',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'category': 'Manufacturing',
     'summary': '',
     'author': 'Pledra, Willdooit, Quartile Limited',
     'license': 'LGPL-3',
-    'website': 'https://www.odoo-asia.com',
+    'website': 'https://www.quartile.co',
     'depends': [
         'product_configurator_mrp',
     ],
     "data": [
         'security/ir.model.access.csv',
-        'views/product_template_views.xml',
+        'views/product_attribute_value_set_views.xml',
     ],
     'installable': True,
 }
