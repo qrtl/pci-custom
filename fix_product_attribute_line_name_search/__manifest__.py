6c2bd9405dcaf59cc78a8159430876df684d2199
Yoshi Tashiro <tashiro@quartile.co>
2018-08-26 13:30:15 +0000

    change to use expression.AND

--- fix_product_attribute_line_name_search/__manifest__.py
+++ fix_product_attribute_line_name_search/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Temporary Fix for Product Attribute Lime name_search',
-    'version': '10.0.1.0.1',
+    'version': '10.0.1.0.2',
     'category': 'Other',
     'summary': '',
     'author': 'Quartile Limited',
