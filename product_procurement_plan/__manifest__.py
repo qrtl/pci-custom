b94f999d80a553c37e58cc1d53a27079fa012195
Yoshi Tashiro <tashiro@quartile.co>
2018-10-05 16:17:56 +0000

    misc. cleanup

--- product_procurement_plan/__manifest__.py
+++ product_procurement_plan/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Rooms For (Hong Kong) Limited T/A OSCG
+# Copyright 2017-2018 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     'name': 'Product Procurement Planning',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'category': 'Product',
     'summary': '',
     'description': """
@@ -13,7 +13,7 @@ Main Features
 2. Adds a menu item to open a wizard to run updates on procurement related fields in product.
 3. Adds a menu item to open a product tree view tailored to show procurement related info.
      """,
-    'author': 'Rooms For (Hong Kong) T/A OSCG',
+    'author': 'Quartile Limited',
     'license': 'LGPL-3',
     'depends': [
         'purchase',
@@ -26,5 +26,4 @@ Main Features
         'wizard/product_procurement_view.xml',
     ],
     'installable': True,
-    'active': False,
 }
