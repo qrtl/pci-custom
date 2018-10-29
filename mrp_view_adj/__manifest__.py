c1abf3affa43405a648fde3e1a63ef53afc37d9b
Tim <tl@quartile.co>
2018-10-29 16:13:30 +0800

    [ADD] mrp_view_adj (#127)

---
+++ mrp_view_adj/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Adjustments on MRP Production's views",
+    "summary": "",
+    "description": """
+- Add remarks and location_src_id to list view and search view of MRP.
+""",
+    "version": "10.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://www.quartile.co",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "sale_order_line_mrp_remarks",
+    ],
+    "data": [
+        "views/mrp_production_views.xml",
+    ],
+}
