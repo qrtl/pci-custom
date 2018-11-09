10e393154dc1d988feb0934c79472ae12139b559
Yoshi Tashiro <tashiro@quartile.co>
2018-11-09 16:21:21 +0000

    [FIX] remove domain from stock move product_id

---
+++ product_configurator_adj/models/stock_move.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    #product_id = fields.Many2one(domain=[('config_ok', '=', False)])
+    # remove domain because configurable product cannot be selected in stock
+    # moves (inventory transfer), otherwise.
+    product_id = fields.Many2one(domain=[])
