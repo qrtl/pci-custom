eef685f71dc0b2ed2f4d374dbd7c767b8be82699
Yoshi Tashiro <tashiro@quartile.co>
2018-11-10 01:22:23 +0900

    [FIX] remove domain from stock move product_id (#128)

--- product_configurator_adj/__manifest__.py
+++ product_configurator_adj/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Adjustments on Product Configurator Base',
-    'version': '10.0.1.2.0',
+    'version': '10.0.1.2.1',
     'category': 'Generic Modules/Base',
     'summary': '',
     'author': 'Quartile Limited',
