5527fb33f7c8a4b3c302498d44f4b3c1aef9ced5
Tim <tl@quartile.co>
2019-04-03 11:42:10 +0800

    [IMP] Pass the fields through _prepare_mo_vals (#134)
    * [IMP] Pass the fields through _prepare_mo_vals
    
    * [IMP] Improve _prepare_mo_vals logic

--- sale_order_line_mrp_remarks/models/__init__.py
+++ sale_order_line_mrp_remarks/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from . import mrp_production
-from . import sale_order
+from . import procurement_order
 from . import sale_order_line
---
+++ sale_order_line_mrp_remarks/models/procurement_order.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models, fields, api
+
+
+class ProcurementOrder(models.Model):
+    _inherit = 'procurement.order'
+
+    def _prepare_mo_vals(self, bom):
+        res = super(ProcurementOrder, self)._prepare_mo_vals(bom)
+        proc = self
+        while proc != False:
+            if proc.move_dest_id and \
+                    proc.move_dest_id.raw_material_production_id:
+                res['remarks'] = \
+                    proc.move_dest_id.raw_material_production_id.remarks
+                break
+            elif proc.sale_line_id:
+                res['remarks'] = proc.sale_line_id.remarks
+                break
+            elif proc.move_dest_id:
+                proc = proc.move_dest_id.procurement_id
+            else:
+                proc = False
+        return res
