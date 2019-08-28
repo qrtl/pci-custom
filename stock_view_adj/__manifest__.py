b94b145e9edddce7d757f31746def38e7201e01b
Tomoko Matsumura <45782518+nakometal@users.noreply.github.com>
2019-08-28 11:27:28 +0900

    #481 Review and adjust access right settings (#158)
    * [ADD] custom action_done button on the header
    * [IMP] manifest

--- stock_view_adj/__manifest__.py
+++ stock_view_adj/__manifest__.py
@@ -2,17 +2,21 @@
 # Copyright 2017 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    'name': 'Disable Force Availability',
-    'version': '10.0.1.0.0',
+    'name': 'Stock View Adjust',
+    'version': '10.0.1.1.0',
     'author': 'Quartile Limited',
-    'website': 'https://www.odoo-asia.com',
+    'website': 'https://www.quartile.co',
     'category': 'Stock',
     'license': "LGPL-3",
-    'description': "",
+    'description': """
+- Stock Inventory View : Display the validation button when operated by Manager and Users.
+- Stock Picking View : Disable Force Availability.
+    """,
     'depends': [
         'stock',
     ],
     'data': [
+        'views/stock_inventory_views.xml',
         'views/stock_picking_views.xml',
     ],
     'installable': True,
