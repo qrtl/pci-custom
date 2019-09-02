38f0cc8837309a4d91cbe62997aa534e4076e98c
Tomoko Matsumura <tomoko.m.0123@gmail.com>
2019-09-02 17:23:02 +0800

    [REL] 10.0.1.2.0

--- stock_view_adj/__manifest__.py
+++ stock_view_adj/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     'name': 'Stock View Adjust',
-    'version': '10.0.1.1.0',
+    'version': '10.0.1.2.0',
     'author': 'Quartile Limited',
     'website': 'https://www.quartile.co',
     'category': 'Stock',
