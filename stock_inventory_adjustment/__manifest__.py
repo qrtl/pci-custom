791b0dd0a859cd993ae8c87a96e4c85b916817d2
Tomoko Matsumura <45782518+nakometal@users.noreply.github.com>
2019-09-04 09:58:44 +0900

    #490 Add Editable Note field in Inventory Adjustment (#160)
    * [ADD] editable Note field

---
+++ stock_inventory_adjustment/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Stock View Adjust',
+    'version': '10.0.1.1.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'category': 'Stock',
+    'license': "LGPL-3",
+    'description': """
+- Adjustment Inventory Adjustment
+    - Add note field.
+    """,
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/stock_inventory_views.xml',
+    ],
+    'installable': True,
+}
