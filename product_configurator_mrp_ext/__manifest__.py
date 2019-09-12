4c0fdce592c6a0ef58f601f237374fa3ebf9011c
Yoshi Tashiro <tashiro@quartile.co>
2019-09-12 18:04:21 +0900

    433 imp product configurator mrp ext (#162)
    * add part_category in config product mapping
    
    * update comments
    
    * add group by definitions

--- product_configurator_mrp_ext/__manifest__.py
+++ product_configurator_mrp_ext/__manifest__.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016-2017 Pledra
 # Copyright 2017 Willdooit
-# Copyright 2017-2018 Quartile Limited
+# Copyright 2017-2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Product Configurator Manufacturing Extended',
-    'version': '10.0.1.1.1',
+    'version': '10.0.1.2.0',
     'category': 'Manufacturing',
     'summary': '',
     'author': 'Pledra, Willdooit, Quartile Limited',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'website': 'https://www.quartile.co',
     'depends': [
         'product_configurator_mrp',
+        'mrp_specsheet_py3o',
     ],
     "data": [
         'security/ir.model.access.csv',
