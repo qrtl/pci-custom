d268a058940aa8b7e36052ad4d8887be8927d915
Yoshi Tashiro <tashiro@quartile.co>
2019-10-02 21:42:29 +0800

    update description

--- stock_move_status_change/__manifest__.py
+++ stock_move_status_change/__manifest__.py
@@ -5,7 +5,8 @@
     "name": "Stock Move Status Change",
     "summary": "",
     "description": """
-Adds functions to change the status of stock move records.
+Adds functions to change the status of stock move records to give users 
+some options to adjust manufacturing order contents as necessary.
 """,
     "version": "10.0.1.0.0",
     "category": "Inventory",
