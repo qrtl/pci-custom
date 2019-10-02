b14effc4245f13d0f832ef5591b42e3d6b62ecd7
Yoshi Tashiro <tashiro@quartile.co>
2019-10-02 22:43:24 +0900

    433 adjust manufacturing order form (#165)
    * adjust manufacturing order form
    
    * add stock_move_status_change
    
    * add action buttons to stock move form
    
    * update description

---
+++ stock_move_status_change/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Move Status Change",
+    "summary": "",
+    "description": """
+Adds functions to change the status of stock move records to give users 
+some options to adjust manufacturing order contents as necessary.
+""",
+    "version": "10.0.1.0.0",
+    "category": "Inventory",
+    "website": "https://www.quartile.co",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/stock_move_views.xml",
+    ],
+}
