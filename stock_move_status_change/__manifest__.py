d1978b5a4def70e5b18612f33fb8b8c049c71ea1
Yoshi Tashiro <tashiro@quartile.co>
2019-10-02 19:26:08 +0800

    add stock_move_status_change

---
+++ stock_move_status_change/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018-2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Move Status Change",
+    "summary": "",
+    "description": """
+Adds functions to change the status of stock move records.
+""",
+    "version": "10.0.1.0.0",
+    "category": "Inventory",
+    "website": "https://www.quartile.co",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_move_views.xml",
+    ],
+}
