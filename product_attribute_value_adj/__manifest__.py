eb93726b0088888f81959f4c8de16f893b6cf923
Sokream Phan <sokream@quartile.co>
2019-11-11 10:56:41 +0800

    [FIX] disable 'Duplicate' button on Attribute Value

---
+++ product_attribute_value_adj/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Adjustment on Product Attribute Value',
+    'version': '10.0.1.0.0',
+    'category': 'Generic Modules/Base',
+    'summary': 'Disable the "Duplicate" Button on Product Attribute Value',
+    'author': 'Quartile Limited',
+    'license': 'AGPL-3',
+    'website': 'https://www.quartile.co',
+    'depends': [
+        'product_configurator',
+    ],
+    "data": [
+        'views/product_attribute_value_views.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
