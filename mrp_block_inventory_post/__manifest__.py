4a349b671f1acf8a166a06ba4b604787fb16af58
Yoshi Tashiro <tashiro@quartile.co>
2019-12-15 22:48:28 +0900

    577 upd mrp view adj (#167)
    * show qty_available_location in MO form
    
    * add button in MO line to show reserving stock moves
    
    * add unreserve button
    
    * add mrp_block_inventory_post
    
    * show qty on hand in manufacturing order lines

--- mrp_block_inventory_post/__manifest__.py
+++ mrp_block_inventory_post/__manifest__.py
@@ -2,23 +2,19 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Stock Move Status Change",
+    "name": "MRP Block Inventory Post",
     "summary": "",
     "description": """
-Adds functions to change the status of stock move records to give users 
-some options to adjust manufacturing order contents as necessary.
+- Adds checking to prevent inventory post for manufacturig order when there
+is an unreserved stock move for components.
 """,
     "version": "10.0.1.1.0",
-    "category": "Inventory",
+    "category": "Manufacturing",
     "website": "https://www.quartile.co",
     "author": "Quartile Limited",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
         "mrp",
-    ],
-    "data": [
-        "data/stock_move_data.xml",
-        "views/stock_move_views.xml",
     ],
 }
