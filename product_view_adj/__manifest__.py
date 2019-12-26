baf148e966d8527c7409b7dce1f0522afac747de
Yoshi Tashiro <tashiro@quartile.co>
2019-12-26 16:32:41 +0800

    bump module version

--- product_view_adj/__manifest__.py
+++ product_view_adj/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Adjustments on Product Related Views',
-    'version': '10.0.1.1.0',
+    'version': '10.0.1.2.0',
     'category': 'Generic Modules/Base',
     'summary': '',
     'author': 'Quartile Limited',
