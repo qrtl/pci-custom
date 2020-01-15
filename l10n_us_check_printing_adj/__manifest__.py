8daccac3e65daba4ae137402bb86abb35c9a04f0
tslai <tl@quartile.co>
2020-01-15 11:09:20 +0800

    [IMP] Remove phone and fax from contact

--- l10n_us_check_printing_adj/__manifest__.py
+++ l10n_us_check_printing_adj/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'US Check Printing Adjustments',
     'version': '10.0.1.0.0',
     'author': 'Quartile Limited',
-    'website': 'https://www.odoo-asia.com',
+    'website': 'https://www.quartile.co',
     'category': 'Localization',
     'license': "AGPL-3",
     'summary': 'Print US Checks',
