41ca10ac1784a241e50162fcdfb110391406d14f
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-06-04 08:14:06 +0530

    [10.0][T991] Add website_sale_comment_category_wise module (#177)

---
+++ website_sale_comment_category_wise/controllers/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import http
+from odoo.http import request
+
+
+class WebsiteSale(http.Controller):
+
+    @http.route(["/shop/order/note"], type="json", auth="public", website=True)
+    def order_note(self, note, **post):
+        order = request.website.sudo().sale_get_order()
+        if order:
+            order.sudo().note = note
---
+++ website_sale_comment_category_wise/models/product_public_category.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class ProductPublicCategory(models.Model):
+    _inherit = "product.public.category"
+
+    website_order_comment = fields.Boolean(
+        string="Website Order Comment",
+        copy=False,
+    )
