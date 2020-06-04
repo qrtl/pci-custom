39cc50fdec08c04a3a8a7972218b3bd841c00a30
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-06-04 08:14:06 +0530

    [FIX] Fix Error on Copy database and PCIDB

---
+++ website_sale_comment_category_wise/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Website Sale Comment Category wise",
+    "version": "10.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Website",
+    "license": "LGPL-3",
+    "depends": [
+        "website_sale",
+    ],
+    "data": [
+        "views/templates.xml",
+        "views/product_public_category_views.xml",
+    ],
+    "installable": True,
+}
---
+++ website_sale_comment_category_wise/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+This module does the following:
+
+* Allows the manage comment box product category wise.
+* Allows to leave a message on confirming an order from website.
