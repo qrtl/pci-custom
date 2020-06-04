bef5a28d3ebce4fd81ad1af292364b994e7e4469
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-06-04 08:14:06 +0530

    [10.0][T991] Add website_sale_comment_category_wise module (#177)

---
+++ website_sale_comment_category_wise/static/src/js/website_payment_note.js
@@ -0,0 +1,12 @@
+odoo.define("website_sale_comment_category_wise.payment", function(require) {
+    "use strict";
+
+    var ajax = require("web.ajax");
+    $(document).ready(function () {
+        $("#order_comment").find("[name='note']").on("change", function () {
+            ajax.jsonRpc("/shop/order/note", "call", {
+                'note': $(this).val(),
+            });
+        });
+    });
+});
