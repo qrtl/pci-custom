331a22f0d1ac20b0b4e224b2da0ecfa2f265e0e2
Tim <tl@quartile.co>
2020-06-08 14:58:58 +0800

    [1021][IMP] Change free_delivery to free_fix_delivery (#179)

--- website_product_delivery_method/__manifest__.py
+++ website_product_delivery_method/__manifest__.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Quartile Limited
+# Copyright 2017-2020 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Website Product Delivery Method',
     'summary': 'Website Product Delivery Method',
-    'version': '10.0.1.0.1',
+    'version': '10.0.1.1.0',
     'license': 'AGPL-3',
-    'category':'Sales',
+    'category': 'Sales',
     'description': """
 - Add customer groups to res.partner and filter the delivery method based on \
 customer group of the customer.
-- Add free delivery setting to product category which allows free delivery \
-for the e-commerce sales order if the order only consists of products from \
-free delivery product category
+- Add free/fixed-price delivery setting to product category which limits \
+delivery methods for the e-commerce sales order if the order only consists \
+of products from free/fixed pric delivery product category.
     """,
     'author': 'Quartile Limited',
-    'website': 'https://www.odoo-asia.com',
+    'website': 'https://www.quartile.co',
     'depends': [
         'website_sale_delivery',
     ],
-    'data':[
+    'data': [
         'data/customer_group_data.xml',
         'security/ir.model.access.csv',
         'views/customer_group_view.xml',
--- website_product_delivery_method/models/product_category.py
+++ website_product_delivery_method/models/product_category.py
@@ -8,7 +8,8 @@ from odoo import fields, models
 class ProductCategory(models.Model):
     _inherit = 'product.category'
 
-    free_delivery = fields.Boolean(
-        string="Free Delivery",
+    free_fix_delivery = fields.Boolean(
+        string="Free/Fixed-price Delivery",
         copy=False,
+        oldname="free_delivery",
     )
