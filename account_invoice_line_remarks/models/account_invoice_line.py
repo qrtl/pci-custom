20ee174939b63b59e43f763ef54f26284e265730
tslai <tl@quartile.co>
2020-12-17 22:39:38 +0800

    [ADD] Black, isort

--- account_invoice_line_remarks/models/account_invoice_line.py
+++ account_invoice_line_remarks/models/account_invoice_line.py
@@ -2,7 +2,7 @@
 # Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models, api
+from odoo import api, fields, models
 
 
 class AccountInvoiceLine(models.Model):
