378dd37be7af19b6a65898e5a43336417d35088d
tslai <tl@quartile.co>
2020-12-17 22:38:31 +0800

    [IMP] Add remarks to sale report

--- sale_order_line_mrp_remarks/__manifest__.py
+++ sale_order_line_mrp_remarks/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 Quartile Limited
+# Copyright 2018-2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Remarks for Manufacturing Order",
@@ -8,7 +8,7 @@
 - Adds remark field in sales order line.
 - Pass the remark field to corresponding manufacturing orders.
 """,
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.0.1",
     "category": "Manufacturing",
     "website": "https://www.quartile.co",
     "author": "Quartile Limited",
@@ -19,6 +19,7 @@
         "mrp",
     ],
     "data": [
+        "report/sale_report_templates.xml",
         "views/sale_order_views.xml",
         "views/mrp_production_views.xml",
     ],
