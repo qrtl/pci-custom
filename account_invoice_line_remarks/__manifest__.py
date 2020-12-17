4600d90355ec34ad831e5fea8a7ca81bff7e2bc2
tslai <tl@quartile.co>
2020-12-17 22:38:15 +0800

    [ADD] account_invoice_line_remarks

---
+++ account_invoice_line_remarks/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Remarks for Account Invoice Line",
+    "version": "10.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://www.quartile.co",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "depends": [
+        "sale_order_line_mrp_remarks",
+    ],
+    "data": [
+        "report/invoice_report_templates.xml",
+        "views/account_invoice_line_views.xml",
+    ],
+    "installable": True,
+}
