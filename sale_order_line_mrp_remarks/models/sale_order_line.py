0c7db74e1c57f2d635e0d103482066243bbb0230
Yoshi Tashiro <tashiro@quartile.co>
2020-12-19 12:21:59 +0800

    add condition to show/hide remarks, add readme

--- sale_order_line_mrp_remarks/models/sale_order_line.py
+++ sale_order_line_mrp_remarks/models/sale_order_line.py
@@ -2,7 +2,7 @@
 # Copyright 2018 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SaleOrderLine(models.Model):
@@ -14,3 +14,10 @@ class SaleOrderLine(models.Model):
         states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
         string="Remarks",
     )
+
+    @api.multi
+    def _prepare_invoice_line(self, qty):
+        self.ensure_one()
+        res = super(SaleOrderLine, self)._prepare_invoice_line(qty)
+        res["remarks"] = self.remarks
+        return res
