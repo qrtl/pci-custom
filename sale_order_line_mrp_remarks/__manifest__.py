897f706cca4dd02e11ab35f9b14bfcf94b8e3ecc
Yoshi Tashiro <tashiro@quartile.co>
2020-12-21 14:39:14 +0900

    [1499][IMP] sale_order_line_mrp_remarks (#181)
    * [ADD] account_invoice_line_remarks
    
    * [IMP] Add remarks to sale report
    
    * [ADD] Black, isort
    
    * add condition to show/hide remarks, add readme
    
    * remove comments
    
    * fix remarks field definitions
    
    Co-authored-by: tslai <tl@quartile.co>

--- sale_order_line_mrp_remarks/__manifest__.py
+++ sale_order_line_mrp_remarks/__manifest__.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 Quartile Limited
+# Copyright 2018-2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    "name": "Remarks for Manufacturing Order",
+    "name": "Sale Order Line Remarks",
     "summary": "",
-    "description": """
-- Adds remark field in sales order line.
-- Pass the remark field to corresponding manufacturing orders.
-""",
-    "version": "10.0.1.0.0",
-    "category": "Manufacturing",
+    "version": "10.0.1.1.0",
+    "category": "Sales",
     "website": "https://www.quartile.co",
     "author": "Quartile Limited",
     "license": "LGPL-3",
@@ -19,6 +15,9 @@
         "mrp",
     ],
     "data": [
+        "report/invoice_report_templates.xml",
+        "report/sale_report_templates.xml",
+        "views/account_invoice_views.xml",
         "views/sale_order_views.xml",
         "views/mrp_production_views.xml",
     ],
---
+++ sale_order_line_mrp_remarks/models/account_invoice_line.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    remarks = fields.Char("Remarks", copy=False)
