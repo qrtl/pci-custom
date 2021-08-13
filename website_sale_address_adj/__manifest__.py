1a04dff84001644b58871a8b359ae8d0fc057440
TomohiroAbe-Quartile <84064473+TomohiroAbe-Quartile@users.noreply.github.com>
2021-08-13 18:01:39 +0900

    st1 [ADD][1823] website_sale_checkout_adj (#184)
    - Add xpath for show a message to prompt inputting the fullname below the Name field.
    - Make the Zip Code field mandatory.

---
+++ website_sale_address_adj/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgp1).
+{
+    "name": "Address Adjustments on Website Sale",
+    "version": "10.0.1.0.0",
+    "category": "Website",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "website": "https://www.quartile.co/",
+    "depends": ["website_sale",],
+    "data": ["views/website_sale.xml"],
+    "installable": True,
+}
