55ec0b74c222c84b05d163abe8e50fb035ff6419
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-10-19 10:29:42 +0800

    regenerate readme, update manifest

--- purchase_order_price_unchange/__manifest__.py
+++ purchase_order_price_unchange/__manifest__.py
@@ -2,6 +2,7 @@
 # Copyright 2016-2021 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
+    "name": "Purchase Order Price Unchange",
     "version": "10.0.1.0.0",
     "category": "Sales",
     "website": "https://www.quartile.co/",
