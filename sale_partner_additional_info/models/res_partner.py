0725eea302b9261328e646d7efc85a8cb6a4c208
Rinaldi Firdaus (Quartile) <122415281+rinaldifirdaus@users.noreply.github.com>
2023-03-23 08:26:10 +0700

    [3264][ADD] sale_partner_additional_info (#215)
    ---------
    
    Co-authored-by: Yoshi Tashiro <tashiro@quartile.co>

---
+++ sale_partner_additional_info/models/res_partner.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    additional_info1 = fields.Char(
+        "Additional Info 1",
+        help="The set value will show in printed sales orders / invoices.",
+    )
+    additional_info2 = fields.Char(
+        "Additional Info 2",
+        help="The set value will show in printed sales orders / invoices.",
+    )
+    additional_info3 = fields.Char(
+        "Additional Info 3",
+        help="The set value will show in printed sales orders / invoices.",
+    )
---
+++ sale_partner_additional_info/readme/DESCRIPTION.rst
@@ -0,0 +1,14 @@
+This module does the following:
+
+- Adds Additional Information fields (1-3) to the partner model, whose values should
+  show in printed sales orders / invoices.
+- Adjust the address presentation of partners in printed sales orders / invoices (i.e.
+  show phone), since, by default, there are cases where phone does not show together with
+  the address in printed documents.
+
+Additional Information fields
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+These fields are intended to keep required particulars to show on business documents
+(particularly export documents) to comply with regulations or acknowledged
+conventions/standards.
