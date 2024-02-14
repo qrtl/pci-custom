54fb751cd7fa8ea142cf821d013d98d42b877ed2
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2024-02-14 15:41:32 +0630

    [ADD] purchase_stock_price_unit_sync (#222)

---
+++ purchase_stock_price_unit_sync/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2024 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Showing actual received quantity of Purchase Order',
+    'version': '10.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'category': 'Purchase',
+    'license': "AGPL-3",
+    'depends': ['purchase'],
+    'installable': True,
+}
---
+++ purchase_stock_price_unit_sync/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module updates the price_unit field of the stock move to match the price_unit
+of the purchase order line when the price_unit is modified after the purchase order
+has been confirmed.
